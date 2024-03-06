055d9146dcc77f9fd95ad3b4b966d6d050ef469d
Julian10808 <161330576+Julian10808@users.noreply.github.com>
2024-03-06 10:17:51 +0700

    Update Introduction.js

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -62,7 +62,7 @@ const cryptosETH = stableTokens.cryptosETH;
 const Introduction = () => {
   const calculateTimeLeft = () => {
     const now = new Date().getTime();
-    const difference = 1709856000000 - now;
+    const difference = 1710040782000 - now;
 
     if (difference <= 0) {
       // Countdown has expired
@@ -181,7 +181,7 @@ const Introduction = () => {
 
     // Clear the interval when the component unmounts
     return () => clearInterval(timer);
-  }, [1709856000000]);
+  }, [1710040782000]);
 
   useEffect(() => {
     const intervalId = setInterval(
